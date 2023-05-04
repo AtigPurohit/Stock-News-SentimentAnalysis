e643c8bfc981ba06f0ccd827a30302b983a1f71f
arnav-007 <106928017+arnav-007@users.noreply.github.com>
2023-05-04 21:03:37 +0530

    changed a few files

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ from NewsExtraction import Extraction
 from SentimentAnalysis import SentimentScore
 
 if __name__ == "__main__":
-    company_name = input("Enter the company name -  ")
+    company_name = input("Enter the company name or any topic you want news on -  ")
 
     df = Extraction(company_name)
     print(df)
-
+  #sdgdsfh
     score = SentimentScore(df)
     print(score)
 
